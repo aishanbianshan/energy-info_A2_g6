0dcf41db5aa330d2561d8d8b5cdd4a039e5981f2
fredrikofstad <nokotin@gmail.com>
2024-05-01 03:51:58 +0200

    exports

--- src/q3/train.py
+++ src/q3/train.py
@@ -45,45 +45,60 @@ def ann():
     pred = model.predict(forecast['TIME'].values.reshape(-1, 1))
     fore_df["FORECAST"] = pred
     rmse = np.sqrt(((fore_df["FORECAST"] - solution["POWER"]) ** 2).mean())
-    print("SVR RMSE:", rmse)
+    print("ANN RMSE:", rmse)
     export = fore_df[['TIMESTAMP', 'FORECAST']]
     export.to_csv("export/ForecastTemplate3-ANN.csv", index=False)
 
 
 # Recurrent Neural Network
-def create_sequences(data, window_size):
+def create_sequences(df, target, window_size=5):
     sequences = []
     labels = []
-    for i in range(len(data) - window_size):
-        sequences.append(data[i:(i + window_size)])
-        labels.append(data[i + window_size])
+    for i in range(len(df) - window_size):
+        sequences.append(df.iloc[i:i+window_size][['hour', 'dayofweek', 'month']].values)
+        labels.append(df.iloc[i + window_size][target])
     return np.array(sequences), np.array(labels)
 
 def rnn():
-    window_size = 5
-    rnn_X, rnn_y = create_sequences(time_series['POWER'].values, window_size)
-    rnn_X = rnn_X.reshape((rnn_X.shape[0], rnn_X.shape[1], 1))
+    df = forecast.copy()
+    ts = time_series.copy()
+    ts['TIMESTAMP'] = pd.to_datetime(ts['TIMESTAMP'])
+    ts['hour'] = ts['TIMESTAMP'].dt.hour
+    ts['dayofweek'] = ts['TIMESTAMP'].dt.dayofweek
+    ts['month'] = ts['TIMESTAMP'].dt.month
+
+    window_size = 10
+    X_train, y_train = create_sequences(ts, 'POWER', window_size)
     model = tf.keras.Sequential(
         [
-            tf.keras.layers.LSTM(50, activation='relu', input_shape=(window_size, 1)),
+            tf.keras.layers.SimpleRNN(50, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2])),
             tf.keras.layers.Dense(1)
         ]
     )
     model.compile(optimizer="adam", loss="mean_squared_error")
-    model.fit(rnn_X, rnn_y, epochs=20, batch_size=16)
+    model.fit(X_train, y_train, epochs=30, batch_size=16)
 
-    X_pred, _ = create_sequences(fore_df['TIME'].values, window_size)
-    X_pred = X_pred.reshape((X_pred.shape[0], X_pred.shape[1], 1))
+    df['TIMESTAMP'] = pd.to_datetime(df['TIMESTAMP'])
+    df['hour'] = df['TIMESTAMP'].dt.hour
+    df['dayofweek'] = df['TIMESTAMP'].dt.dayofweek
+    df['month'] = df['TIMESTAMP'].dt.month
+    df['POWER'] = None
+
+    X_pred, _ = create_sequences(df, "POWER", window_size)
 
     pred = model.predict(X_pred)
-    print(pred)
-    fore_df["FORECAST"] = pred
-    rmse = np.sqrt(((fore_df["FORECAST"] - solution["POWER"]) ** 2).mean())
-    print("SVR RMSE:", rmse)
-    export = fore_df[['TIMESTAMP', 'FORECAST']]
+    df = fore_df.iloc[window_size:]
+    sol = solution.iloc[window_size:]
+    df["FORECAST"] = pred
+    rmse = np.sqrt(((df["FORECAST"] - sol["POWER"]) ** 2).mean())
+    print("RNN RMSE:", rmse)
+    export = df[['TIMESTAMP', 'FORECAST']]
     export.to_csv("export/ForecastTemplate3-RNN.csv", index=False)
 
 # Prediction
 
 if __name__ == "__main__":
+    linear()
+    svr()
+    ann()
     rnn()
