d48269c967e2e3da27ca6b9ca8114fcae1e3a379
fredrikofstad <nokotin@gmail.com>
2024-05-07 16:22:28 +0200

    updated plot

--- src/q2/plotting.py
+++ src/q2/plotting.py
@@ -4,9 +4,9 @@ from MLR import *
 
 plt.figure(figsize=(12, 8))
 
-plt.plot(dates, forecast_y, label='True Wind Power', color='green',  alpha=0.8)
-plt.plot(dates, mlr_y_pred, label='MLR Predicted Power', color='navy', alpha=0.7)
-plt.plot(dates, lr_y_pred, label='LR Predicted Power', color='deeppink', alpha=0.7)
+plt.plot(dates, forecast_y, label='True Wind Power')
+plt.plot(dates, mlr_y_pred, label='MLR Predicted Power')
+plt.plot(dates, lr_y_pred, label='LR Predicted Power')
 
 plt.title(f"True Wind Power vs LR and MLR Predicted Power")
 plt.xticks(rotation=45)
